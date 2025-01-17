7f329d134e4903183415146d6287336fe3aeb214
Ahmed Frag <a.frag166@gmail.com>
2025-01-17 03:49:18 +0200

    Validation & Send Opinion to Backend ( by Ahmed )

---
+++ src/validation.js
@@ -0,0 +1,11 @@
+export const isEmpty = (value) => {
+  return value.trim().length === 0;
+};
+
+export const hasMinLength = (value, minLength) => {
+  return value.trim().length < minLength;
+};
+
+export const isNotBetween = (value, minLength, maxLength) => {
+  return value.trim().length < minLength || value.trim().length > maxLength;
+};
